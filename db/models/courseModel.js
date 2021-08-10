5465c8f308e0260d183ef9800bf3d6d029c63c50
Zeus <maver176005@mail.ru>
2021-08-10 22:16:01 +0300

    cur

--- db/models/courseModel.js
+++ db/models/courseModel.js
@@ -1,24 +1,26 @@
 const { Schema, model } = require("mongoose");
 
 const courseSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  lecturesId: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Lectures",
+      required: true,
     },
-    description: {
-        type: String,
-        required: true,
-    },
-    lecturesId: [{
-        type: Schema.Types.ObjectId,
-        ref: "Lectures",
-        required: true,
-    }],
-    teacher: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    }
+  ],
+  teacher: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 module.exports = model("Course", courseSchema);
