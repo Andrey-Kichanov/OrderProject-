7b475752ff24f29e0d472dcac3c584c4af58df1c
Roman <adalaf@yandex.ru>
2021-08-12 18:22:29 +0300

    authorize done

--- routes/authorization.route.js
+++ routes/authorization.route.js
@@ -11,18 +11,18 @@ router
   .post(async (req, res) => {
     try {
       const { emailValue, passValue } = req.body;
-      const checkedUser = await User.findOne({ email: emailValue });
-      
+      const checkedUser = await User.findOne({ email: emailValue, password: passValue });
+      console.log(checkedUser)
       if (!checkedUser) {
         const message = 'Пользователдь не найден, зарегистрируйтесь';
         return res.send(message);
       }
-      if (checkedUser.password !== password) {
+      if (checkedUser.password !== passValue) {
         return res.redirect("/login/?error=incorrectPassword");
       }
 
       req.session.user = checkedUser;
-      res.json({ enter: true });
+      return res.json({ enter: true });
     } catch (error) {
       console.log(error);
       res.sendStatus(418);
