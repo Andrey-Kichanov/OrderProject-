0477648c8402c655336e061c12f0c82d63e862ff
Roman <adalaf@yandex.ru>
2021-08-13 16:18:58 +0300

    Merge

--- routes/aboute.route.js
+++ routes/aboute.route.js
@@ -1,18 +1,18 @@
-const { Router } = require("express");
+const { Router } = require('express');
 
 const router = Router();
-const fs = require("fs").promises;
+const fs = require('fs').promises;
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const aboute = await fs.readFile(
-    "./public/aboutCompany.txt",
-    "utf-8",
+    './public/aboutCompany.txt',
+    'utf-8',
     (data) => {
       console.log(data);
-    }
+    },
   );
   console.log(aboute);
-  res.render("aboute", { aboute });
+  res.render('aboute', { aboute });
 });
 
 module.exports = router;
--- routes/allcourses.route.js
+++ routes/allcourses.route.js
@@ -1,18 +1,12 @@
 const { Router } = require('express');
 const CourseModel = require('../db/models/courseModel');
+
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const allCourses = await CourseModel.find()
+  const allCourses = await CourseModel.find();
 
   res.render('allcourse', { allCourses });
 });
-
-// router.post('/:id', async (req, res) => {
-//   const { id } = req.params;
-//   const currentLectures = await LecturesModel.findById(id);
-
-//   return res.json({ currentLectures });
-// });
 
 module.exports = router;
