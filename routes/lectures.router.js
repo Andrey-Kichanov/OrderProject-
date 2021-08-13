8dc00f8f75f3b5e00ab37bf59c7c1bc353414afd
Andrey-Kichanov <andrey.ki47@GMAIL.COM>
2021-08-13 09:59:53 +0300

    merge v2.2

--- routes/lectures.router.js
+++ routes/lectures.router.js
@@ -5,7 +5,8 @@ const router = Router();
 
 router.get('/:id', async (req, res) => {
   const currentLecture = await LecturesModel.findById(req.params.id);
-  console.log(currentLecture);
-  res.render('lectures', { currentLecture });
+  if (currentLecture.videoLink!="") vlink=currentLecture.videoLink
+  console.log(vlink);
+  res.render('lectures', { currentLecture,  vlink});
 });
 module.exports = router;
